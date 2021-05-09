db72ec9f962511ec66baa8afc5ef0a17edc13400
chirag agrawal <cheeruagrawal@gmail.com>
2021-05-09 22:02:38 +0530

    deploying

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
+  host: "database-2.cq2opjffhoxv.ap-south-1.rds.amazonaws.com",
+  port: "3306",
+  user: "admin",
   password: "password",
   database: "products",
   multipleStatements: true,
